e651e3c0611131de417f6319eed72268e5dc87f0
Murat Karaiev <murat@itsharkz.com>
2025-01-15 13:22:59 +0100

    feat: add tets verification of game date and initial

--- cypress/specs/regression/public-game.js
+++ cypress/specs/regression/public-game.js
@@ -1,38 +1,58 @@
-import publicGame from "../pages/public-game";
+import PublicGame from "../pages/public-game";
 
-describe('Public Game Tests', () => {
-    let gameId;
-    it('Base Page title', () => {
-        cy.visit('/public-game');
-        cy.title().should('eq', 'The Maf Place');
-    });
+describe("Public Game Tests", () => {
+  let gameId;
 
-    it('Url should be /public-game', () => {
-        publicGame.goToPublicGame();
-        cy.url().should('include', '/public-game');
-    });
+  it("Verify base page title", () => {
+    cy.visit("/public-game");
+    cy.title().should("eq", "The Maf Place");
+  });
 
-    it('Create game', () => {
-        //let gameId;
-        publicGame.createGame();
-        cy.get(publicGame.selectors.idGame).invoke('text').then((text) => {
-            gameId = text;
-            cy.log(gameId);
-            cy.get(publicGame.selectors.startGameBtn).should('is.enabled');
-            //.get(`#${gameId}`).should('exist');
-        });
-    });
+  it("Url should be /public-game", () => {
+    PublicGame.goToPublicGame();
+    cy.url().should("include", "/public-game");
+  });
 
-    it('Verification if the game exists by game id', () => {
-        //cy.wait(10000);
-        cy.get(publicGame.selectors.gameSearchTab).click();
-       cy.get(publicGame.selectors.searchGameField).should('be.visible');
-       cy.get(publicGame.selectors.searchGameField).type(gameId);
+  it("Create game", () => {
+    PublicGame.createGame();
+    cy.get(PublicGame.selectors.idGame)
+      .invoke("text")
+      .then((text) => {
+        gameId = text;
+        cy.log(gameId);
+        cy.get(PublicGame.selectors.startGameBtn).should("is.enabled");
+      });
+  });
 
-       cy.get(publicGame.selectors.searchGameButton).click();
-       //cy.wait(10000);
-       cy.get(publicGame.selectors.continueGameBtn).should('exist')
-       cy.get(publicGame.selectors.resultPhase).should('have.text', 'initial 0');
+  it("Verification if the game exists by game id", () => {
+    cy.get(PublicGame.selectors.gameSearchTab).click();
+    cy.get(PublicGame.selectors.searchGameField).should("be.visible");
+    cy.get(PublicGame.selectors.searchGameField).type(gameId);
+    cy.get(PublicGame.selectors.searchGameButton).click();
+    cy.get(PublicGame.selectors.continueGameBtn).should("exist");
+  });
 
-    });
+  it("Verification of initial game", () => {
+    cy.get(PublicGame.selectors.resultPhase)
+      .invoke("text")
+      .then((text) => {
+        const result = text.trim(); // Trim any extra whitespace
+        cy.log("Result phase:", result);
+        expect(result).to.match(/^initial \d+$/); // Match "initial" followed by a number
+      });
+  });
+
+  it("Verification of date of game", () => {
+    const currentDate = new Date();
+    const formattedCurrentDate = `${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/${currentDate.getDate()}`;
+
+    cy.get(PublicGame.selectors.resultDate)
+      .invoke("text")
+      .then((text) => {
+        const dateGame = text.trim(); // Trim any extra whitespace
+        cy.log("Date of game:", dateGame);
+        cy.log("Current date:", formattedCurrentDate);
+        expect(dateGame).to.equal(formattedCurrentDate);
+      });
+  });
 });
